ad87b29c1a1d61211636059d57e90335fdf3c6f7
Varshini Balakrishnan <varshini.balakrishnan@student.tdsb.on.ca>
2026-01-19 22:48:52 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,28 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+		String[] moves = towerOfHanoi(3);
+		for (int i = 0; i < moves.length; i++) {
+			System.out.println(moves[i]);
+		}
 
 	}
-
-	public static void q1() {
-		//Write question 1 code here
+	public static String[] towerOfHanoi(int num) {
+		int size = (int)Math.pow(2, num) - 1;
+		String[] result = new String[size];
+		int[] index = new int[1];
+		hanoiHelper(num, "L", "M", "R", result, index);
+		return result;
 	}
+	public static void hanoiHelper(int num, String from, String middle, String to, String[] result, int[] index) {
+		if (num == 0) {
+			return;
+		}
+		hanoiHelper(num - 1, from, to, middle, result, index);
+		result[index[0]] = from + to;
+		index[0]++;
+		hanoiHelper(num - 1, middle, from, to, result, index);
 
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
 	}
 
 }
